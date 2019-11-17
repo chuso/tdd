151d027110d56d4d3330904189bbe939a5d58dd7
Jesús Ruiz-Ayúcar <chusete@gmail.com>
2019-11-17 22:14:24 +0100

    Validate that a Draught can only eat at most 1 opponent piece

--- draughts/src/main/java/es/urjccode/mastercloudapps/adcs/draughts/models/Draught.java
+++ draughts/src/main/java/es/urjccode/mastercloudapps/adcs/draughts/models/Draught.java
@@ -1,5 +1,7 @@
 package es.urjccode.mastercloudapps.adcs.draughts.models;
 
+import java.util.List;
+
 class Draught extends Piece {
 
     Draught(Color color) {
@@ -9,6 +11,29 @@ class Draught extends Piece {
     Coordinate getEatenCoordinate(Coordinate origin, Coordinate target) {
         // TODO
         return null;
+    }
+
+    Error isCorrect(Coordinate origin, Coordinate target, PieceProvider pieceProvider) {
+        Error error = super.isCorrect(origin, target, pieceProvider);
+        if (error != null) {
+            return error;
+        }
+        if (numberOfEats(origin, target, pieceProvider) > 1) {
+            return Error.EATING_TOO_MUCH;
+        }
+        return null;
+    }
+
+    private int numberOfEats(Coordinate origin, Coordinate target, PieceProvider pieceProvider) {
+        List<Coordinate> coordinatesBetween = origin.between(target);
+        int differentColorPieces = 0;
+        for (Coordinate coordinate : coordinatesBetween) {
+            Piece piece = pieceProvider.getPiece(coordinate);
+            if (piece != null && piece.getColor() != this.getColor()) {
+                differentColorPieces++;
+            }
+        }
+        return differentColorPieces;
     }
 
 }
